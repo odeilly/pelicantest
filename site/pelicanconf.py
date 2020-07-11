98c669a2f4fdf3cc123d1058c08886fc89cf716a
Takashi Odaira <odaira@insightech.co.jp>
2020-07-11 22:48:24 +0900

    Change theme

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -34,6 +34,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-#THEME = 'themes/voidy-bootstrap'
+THEME = 'themes/voidy-bootstrap'
+STYLESHEET_FILES = ("pygment.css", "custom.css",)
 
 
